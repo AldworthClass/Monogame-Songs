f2ca042eb57ce4fec4f88dbf93f173d110411615
Steve Aldworth <s.aldworth@tvdsb.ca>
2022-05-05 09:33:42 -0400

    Buttons drawn and for resized.

--- Game1.cs
+++ Game1.cs
@@ -30,6 +30,8 @@ namespace Monogame_Songs
         Rectangle muteButtonRect;
         Texture2D unMuteButtonTexture;
         Rectangle unMuteButtonRect;
+        Texture2D restartButtonTexture;
+        Rectangle restartButtonRect;
 
         public Game1()
         {
@@ -41,7 +43,9 @@ namespace Monogame_Songs
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            _graphics.PreferredBackBufferWidth = 265;
+            _graphics.PreferredBackBufferHeight = 300;
+            _graphics.ApplyChanges();
             base.Initialize();
         }
 
@@ -52,12 +56,21 @@ namespace Monogame_Songs
             // TODO: use this.Content to load your game content here
             tngSong = Content.Load<Song>("tng_intro_theme");
             playButtonTexture = Content.Load<Texture2D>("play_button");
+            playButtonRect = new Rectangle(140, 40, 50, 50);
             stopButtonTexture = Content.Load<Texture2D>("stop_button");
+            stopButtonRect = new Rectangle(75, 40, 50, 50);
             muteButtonTexture = Content.Load<Texture2D>("mute_button");
+            muteButtonRect = new Rectangle(10, 105, 50, 50);
             unMuteButtonTexture = Content.Load<Texture2D>("unmute_button");
+            unMuteButtonRect = new Rectangle(205, 105, 50, 50);
             pauseButtonTexture = Content.Load<Texture2D>("pause_button");
+            pauseButtonRect = new Rectangle(205, 40, 50, 50);
             volumeUpButtonTexture = Content.Load<Texture2D>("volume_up_button");
+            volumeUpButtonRect = new Rectangle(140, 105, 50, 50);
             volumeDownButtonTexture = Content.Load<Texture2D>("volume_down_button");
+            volumeDownButtonRect = new Rectangle(75, 105, 50, 50);
+            restartButtonTexture = Content.Load<Texture2D>("restart_button");
+            restartButtonRect = new Rectangle(10, 40, 50, 50);
         }
 
         protected override void Update(GameTime gameTime)
@@ -75,6 +88,18 @@ namespace Monogame_Songs
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(restartButtonTexture, restartButtonRect, Color.White);
+            _spriteBatch.Draw(stopButtonTexture, stopButtonRect, Color.White);
+            _spriteBatch.Draw(playButtonTexture, playButtonRect, Color.White);
+            _spriteBatch.Draw(pauseButtonTexture, pauseButtonRect, Color.White);
+
+            _spriteBatch.Draw(muteButtonTexture, muteButtonRect, Color.White);
+            _spriteBatch.Draw(unMuteButtonTexture, unMuteButtonRect, Color.White);
+            _spriteBatch.Draw(volumeUpButtonTexture, volumeUpButtonRect, Color.White);
+            _spriteBatch.Draw(volumeDownButtonTexture, volumeDownButtonRect, Color.White);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
